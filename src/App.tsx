01d11f511f117a379b4f810c187a0abd1f560643
exy <teslawiktor@gmail.com>
2026-02-12 20:31:35 +0100

    - Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { AboutMe } from "./components/AboutMe";
 import { Contact } from "./components/Contact";
 import { Home } from "./components/Home";
+import { Navigation } from "./components/Navigation";
 import { Works } from "./components/Works";
 
 function App() {
   return (
     <>
-      <div>
-        <Home />
-      </div>
+      <Navigation />
       <div>
         <Home />
       </div>
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,7 @@
+export function Navigation() {
+  return (
+    <>
+      <p>Navigation</p>
+    </>
+  );
+}
