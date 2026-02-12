79b290f73b0df66ada8383a5cb78fecd3b6eb27d
exy <teslawiktor@gmail.com>
2026-02-12 20:15:07 +0100

    - Project Structure - Home.tsx - Works.tsx - AboutMe.tsx - Contact.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { AboutMe } from "./components/AboutMe";
+import { Contact } from "./components/Contact";
+import { Home } from "./components/Home";
+import { Works } from "./components/Works";
+
 function App() {
   return (
     <>
-      <p className="text-2xl">Hi</p>
+      <div>
+        <Home />
+      </div>
+      <div>
+        <Home />
+      </div>
+      <div>
+        <Works />
+      </div>
+      <div>
+        <AboutMe />
+      </div>
+      <div>
+        <Contact />
+      </div>
     </>
   );
 }
---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,7 @@
+export function AboutMe() {
+  return (
+    <>
+      <p>About me</p>
+    </>
+  );
+}
